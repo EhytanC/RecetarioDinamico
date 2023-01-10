e37aed9092050ca02405f27be7f71fc7943c1072
ehytanc <ehytan.correo@gmail.com>
2023-01-10 14:52:06 -0400

    optimizando codigo y agregando mas endpoints

--- api/recursos/ingredientes.py
+++ api/recursos/ingredientes.py
@@ -1,30 +1,32 @@
 import pandas as pd
 from esquemas.models import ingredient
-from recursos.recetas_ingredientes import buscador_ingredientes, transform_item
+from recursos.recetas_ingredientes import transform_item, buscador_ingredientes
 from fastapi import APIRouter, HTTPException 
 df_ingredients = pd.read_csv('../db/ingredient_test.csv')
 
 ingredient_point = APIRouter()
 
-#@ingredient_point.get('/ingredientes')
-#def get_ingredients():
-#    return df_ingredients['name'].tail(30).to_dict()
-
-@ingredient_point.get('/ingredientes/{id_receta}')
-def get_list_ingredients(id_receta:int):
+@ingredient_point.get('/ingredientes')
+def get_ingredients(id_receta: int | None = None):
     df = pd.DataFrame()
-    id_ingredientes = buscador_ingredientes(id_receta)
-    for i in id_ingredientes:
-        df = pd.concat([df_ingredients[df_ingredients['id'] == i], df])
-    return df['name'].to_dict()
+    
+    if id_receta != None:
+        id_ingredientes = buscador_ingredientes(id_receta)
+    
+        for i in id_ingredientes:
+            df_res = df_ingredients[df_ingredients['id'] == i]
+            df = pd.concat([df,df_res])        
+    
+    return df.to_dict('list')
 
-@ingredient_point.post('/ingredientes')
+@ingredient_point.post('/ingredientes', status_code=201)
 def post_ingredients(info_ingredient: ingredient):
     df_ingredients = pd.read_csv('../db/ingredient_test.csv')
     
     df = transform_item(info_ingredient)
     df = pd.concat([df_ingredients, df])
     df.to_csv('../db/ingredient_test.csv', index=False)
+    
     return df.tail().to_dict()
 
 @ingredient_point.delete('/ingredientes/{id_ingrediente}')
@@ -36,3 +38,18 @@ def delete_ingredients(id_ingrediente: int):
     df.to_csv('../db/ingredient_test.csv', index=False)
 
     return 'El ingrediente ha sido borrado con exito'
+
+@ingredient_point.put('/ingredientes/{id_ingrediente}')
+def pull_ingredients(info_ingrediente:ingredient):
+    df_ingredients = pd.read_csv('../db/ingredient_test.csv')
+    ingredient_query = dict(info_ingrediente)
+    id_query = df_ingredients[df_ingredients['id']==ingredient_query['id']].index.values
+    
+    if len(id_query) == 0:
+        raise HTTPException(status_code=404, detail='El ingrediente ingresado no existe')
+    
+    for i in ingredient_query:
+        df_ingredients.at[id_query[0],i] = ingredient_query[i]
+    df_ingredients.to_csv('../db/ingredient_test.csv', index=False)
+    
+    return df_ingredients.loc[id_query,'name']
--- api/recursos/recetas.py
+++ api/recursos/recetas.py
@@ -2,24 +2,30 @@ import pandas as pd
 from fastapi import APIRouter, HTTPException
 from esquemas.models import recipe
 from recursos.recetas_ingredientes import transform_item
+from recursos.ingredientes import get_ingredients
 recipe_point = APIRouter()
 
 df_recipes = pd.read_csv('../db/recipes_test.csv')
 
-#@recipe_point.get('/recetas')
-#def get_recetas():
-#    a = df_recipes.head()
-#    print(a.to_dict)
-#    return a.to_dict()
+@recipe_point.get('/recetas')
+def get_recetas(name: str|None=None,):
+    if name != None:
+        df = df_recipes[df_recipes['name'].str.contains(name)].head(25)
+    else:
+        df = df_recipes.tail()
+    return df.to_dict('list')
 
 @recipe_point.get('/recetas/{id_receta}')
-def get_recetas(id_receta:int):
+def get_recetas(id_receta:int, ingredientes:bool | bool = False):
     df = df_recipes[df_recipes['id'] == id_receta]
-    
     if df.empty:
         raise HTTPException(status_code=404, detail='La receta no existe o no fue encontrada')
-    
-    return df.to_dict()
+    elif ingredientes:
+        df = df.to_dict('list')
+        df['ingredientes'] = get_ingredients(id_receta)
+        return df
+    df = df.to_dict('list')
+    return df
 
 @recipe_point.post('/recetas', status_code=201)
 def post_recetas(info_recipe:recipe):
@@ -34,7 +40,7 @@ def post_recetas(info_recipe:recipe):
     df = pd.concat([df_recipes, df])
     df.to_csv('../db/recipes_test.csv', index=False)
 
-    return df.tail().to_dict()
+    return df.tail().to_dict('list')
 
 @recipe_point.delete('/recetas/{id_receta}')
 def delete_recetas(id_receta:int):
@@ -50,8 +56,10 @@ def delete_recetas(id_receta:int):
 def pull_recetas(info_recipe:recipe):
     df_recipes = pd.read_csv('../db/recipes_test.csv')
     recipe_query = dict(info_recipe)
+    print(recipe_query)
     id_query = df_recipes[df_recipes['id'] == recipe_query['id']].index.values
+    
     for i in recipe_query:
-        df_recipes.at[id_query[-1],i] = recipe_query[i]
+        df_recipes.at[id_query[0],i] = recipe_query[i]
     df_recipes.to_csv('../db/recipes_test.csv', index=False)
-    return df_recipes.loc[recipe_query['id']].to_dict()
+    return df_recipes.loc[id_query[0],'name']
